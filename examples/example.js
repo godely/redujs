c4a4635ec3c46a279d09d4b45faeea666e5f629e
Gustavo Stor <gsa2@cin.ufpe.br>
2014-12-17 09:54:41 -0300

    Fixed issue with constructor params

--- examples/example.js
+++ examples/example.js
@@ -5,7 +5,7 @@ var redu = new Redu("consumer_key", "consumer_secret", "http://base_url/");
 redu.initClient("pin", function(error, access_token) {
   if (!error) {
     console.log("Você está conectado e pode começar a utilizar a API.");
-    redu.getMe({id: 2}, function(object) {
+    redu.getUser({id: 2}, function(object) {
       //object é um user com id 2
     });
   }
